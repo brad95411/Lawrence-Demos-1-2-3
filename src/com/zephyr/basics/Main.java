f83787592706d223499ac52531d133eba840f2df
brad95411 <bradleybickford@gmail.com>
2016-08-01 15:00:31 -0400

    Initial Commit

---
+++ src/com/zephyr/basics/Main.java
@@ -0,0 +1,158 @@
+package com.zephyr.basics;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args)
+	{
+		Scanner scan = new Scanner(System.in);
+		
+		//Demo 1
+		//System.out.println("Hello World!");
+		
+		/*Demo 2
+		int var = 1234;
+		
+		double var2 = 1.0/4;
+		
+		boolean var3 = true;
+		
+		String var4 = "This is a new String!";
+		
+		System.out.println(var);
+		System.out.println(var2);
+		System.out.println(var3);
+		System.out.println(var4);
+		*/
+		
+		//Demo 2
+//		System.out.println("Enter a value for String 1!");
+//		String str1 = scan.nextLine();
+//		System.out.println("Enter a value for String 2!");
+//		String str2 = scan.nextLine();
+		
+//		System.out.println("Enter a number for the universe value!");
+//		int universe = scan.nextInt();
+//		System.out.println("Enter a number for the clues value!");
+//		int clues = scan.nextInt();
+//		
+//		System.out.println("Enter a decimal number for the peanut value!");
+//		double peanut = scan.nextDouble();
+//		System.out.println("Enter a decimal number for the butter value!");
+//		double butter = scan.nextDouble();
+//		
+//		System.out.println("Enter a boolean for the thisValue value!");
+//		boolean thisValue = scan.nextBoolean();
+//		System.out.println("Enter a boolean for the thatValue value!");
+//		boolean thatValue = scan.nextBoolean();
+		
+//		if(str1.equals(str2))
+//		{
+//			System.out.println("String 1 is equal to String 2!");
+//		}
+//		else
+//		{
+//			System.out.println("String 1 is not equal to String 2!");
+//		}
+//		
+//		if(str1 == str2)
+//		{
+//			System.out.println("The memory location for String 1 is the same as String 2!");
+//		}
+//		else
+//		{
+//			System.out.println("The memory location for String 1 is not the same as String 2!");
+//		}
+//		
+//		str1 = str2;
+//		
+//		if(str1 == str2)
+//		{
+//			System.out.println("The memory location for String 1 is the same as String 2!");
+//		}
+//		else
+//		{
+//			System.out.println("The memory location for String 1 is not the same as String 2!");
+//		}
+		
+//		if (universe == clues)
+//		{
+//			System.out.println("Universe is equal to clues!");
+//		}
+//		else
+//		{
+//			System.out.println("Universe is not equal to clues!");
+//		}
+//		
+//		if (peanut == butter)
+//		{
+//			System.out.println("Peanut is equal to butter!");
+//		}
+//		else if (peanut > butter)
+//		{
+//			System.out.println("Peanut is greater than butter!");
+//		}
+//		else // peanut < butter
+//		{
+//			System.out.println("Peanut is less than butter!");
+//		}
+//		
+//		if(thisValue == thatValue)
+//		{
+//			System.out.println("ThisValue is equal to ThatValue!");
+//		}
+//		else
+//		{
+//			System.out.println("ThisValue is not equal to ThatValue!");
+//		}
+//		
+//		if(thisValue == !thatValue)
+//		{
+//			System.out.println("ThisValue is equal to NOT(!) ThatValue!");
+//		}
+//		else
+//		{
+//			System.out.println("ThisValue is not equal to NOT(!) ThatValue!");
+//		}
+//		
+//		if(thisValue || thatValue)
+//		{
+//			System.out.println("Either ThisValue OR ThatValue is true, or maybe both are true!");
+//		}
+//		else
+//		{
+//			System.out.println("Both ThisValue and ThatValue are false");
+//		}
+//		
+//		if(thisValue && (peanut == butter))
+//		{
+//			System.out.println("ThisValue AND peanut == butter is true!");
+//		}
+//		else
+//		{
+//			System.out.println("Either ThisValue or peanut == butter are false, or maybe both are false!");
+//		}
+		
+		//Demo 3
+		/*for(double i = 0.0; i < 5.0; i+=.3)
+		{
+			System.out.println(i);
+		}*/
+	
+//		String cPassword = "Justin";
+//		
+//		String userInput = "";
+//		
+//		while(!cPassword.equals(userInput))
+//		{
+//			System.out.println("Please enter the password!");
+//			
+//			userInput = scan.nextLine();
+//		}
+//		
+//		System.out.println("Your password is correct!");
+		
+		
+	}
+}
